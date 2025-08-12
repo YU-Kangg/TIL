71d92109764967c59c5bb70b481651c71de7a268
YU-Kangg <intyu.kang@gmail.com>
2025-08-13 00:54:04 +0900

    0813 home

--- APS/SWEA/src/Swea12712/Swea12712_0812.java
+++ APS/SWEA/src/Swea12712/Swea12712_0812.java
@@ -6,55 +6,54 @@ public class Swea12712_0812 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		int t = sc.nextInt();
-		for(int tc = 1; tc<=t; tc++) {
+		for (int tc = 1; tc <= t; tc++) {
 			int N = sc.nextInt();
 			int spread = sc.nextInt();
-			
+
 			int[][] arr = new int[N][N];
-			for(int i = 0; i<N; i++) {
-				for(int j =0; j<N; j++) {
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
 					arr[i][j] = sc.nextInt();
 				}
 			}
 			
 			int maxPlus = 0;
 			int maxX = 0;
-			int resultPlus = 0;
-			int resultX = 0;
-			
-			for(int i =0 ; i<spread; i++) {
-				int[] dr = {i, -i, 0, 0};
-				int[] dc = {0, 0, -i, i};
-				int[] dz = {i, i, -i, -i};
-				int[] dy = {i, -i, i, -i};
-				
-				
+			for (int j = 0; j < N; j++) {
+				for (int k = 0; k < N; k++) {
+					int resultPlus = arr[j][k];
+					int resultX = arr[j][k];
+
+					for (int i = 1; i < spread; i++) {
+						int[] dr = { i, -i, 0, 0 };
+						int[] dc = { 0, 0, -i, i };
+						int[] dz = { i, i, -i, -i };
+						int[] dy = { i, -i, i, -i };
+						
+						for (int l = 0; l < 4; l++) {
+							int nr = j + dr[l];
+							int nc = k + dc[l];
+							if (nr >= 0 && nr < N && nc >= 0 && nc < N) {
+								resultPlus += arr[nr][nc];
+							}
+							int nz = j + dz[l];
+							int ny = k + dy[l];
+							if (nz >= 0 && nz < N && ny >= 0 && ny < N) {
+								resultX += arr[nz][ny];
+							}
+						}
+					}
+					if (resultPlus > maxPlus) {
+						maxPlus = resultPlus;
+					}
+					if (resultX > maxX) {
+						maxX = resultX;
+					}
+				}
 			}
-			
-			
-			
-//			for(int i = 0; i<N; i++) {
-//				for(int j =0; j<N; j++) {
-//					resultPlus += arr[i][j];
-//					resultX += arr[i][j];
-//					for(int k =0; k<spread; k++) {
-//						if(i+k>=0 && i+k<N&&i-k>=0&&i-k<N)
-//						resultPlus += arr[i][j+k];
-//						resultPlus += arr[i+k][j];
-//						resultPlus += arr[i-k][j];
-//						resultPlus += arr[i][j-k];
-//					}
-//					if(resultPlus>maxPlus) {
-//						maxPlus = resultPlus;
-//					}
-//				}
-//			}
-			
-			
-			
+			System.out.println("#" + tc + " " + Math.max(maxPlus, maxX));
 		}
 	}
-
 }
